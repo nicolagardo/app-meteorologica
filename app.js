558274db6b475664d3f3a305a05d6f91a26e31bb
nicolagardo <nicolagardo@gmail.com>
2022-05-04 23:46:58 -0300

    apiKey

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert("app.js called")
+//alert("app.js called")
+
+
+
+
+const form = document.querySelector(".top-banner form");
+ 
+form.addEventListener("submit", e => {
+  e.preventDefault()
+  //console.log("test");
+  const apiKey = "627c57232dbfcef21a812fd90aaa2b3f";
+
+  const inputVal = input.value;
+  //console.log(inputVal)
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+});
+
